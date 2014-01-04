0747a8b0d5cfaebb50cddde00d2ae13859565169
Woodhouse <contact@icecave.com.au>
2014-01-04 00:33:56 +0000

    Publishing artifacts from build 17.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -1,12 +1,11 @@
-var tree = [["Icecave",,"",
-                [["Recoil",,"",
-                [["Database","Icecave\/Recoil\/Database.html","",
-                [["Exception","Icecave\/Recoil\/Database\/Exception.html","",
-                [["DatabaseException","Icecave\/Recoil\/Database\/Exception\/DatabaseException.html"," < PDOException",
-                [        ]]        ]],["ConnectionFactory","Icecave\/Recoil\/Database\/ConnectionFactory.html","",
-                [        ]],["ConnectionFactoryInterface","Icecave\/Recoil\/Database\/ConnectionFactoryInterface.html","",
-                [        ]],["ConnectionInterface","Icecave\/Recoil\/Database\/ConnectionInterface.html","",
-                [        ]],["PackageInfo","Icecave\/Recoil\/Database\/PackageInfo.html","",
-                [        ]],["StatementInterface","Icecave\/Recoil\/Database\/StatementInterface.html","",
-                [        ]]        ]]        ]]        ]]        ]
+var tree = [["Recoil",,"",
+                [["Database","Recoil\/Database.html","",
+                [["Exception","Recoil\/Database\/Exception.html","",
+                [["DatabaseException","Recoil\/Database\/Exception\/DatabaseException.html"," < PDOException",
+                [        ]]        ]],["ConnectionFactory","Recoil\/Database\/ConnectionFactory.html","",
+                [        ]],["ConnectionFactoryInterface","Recoil\/Database\/ConnectionFactoryInterface.html","",
+                [        ]],["ConnectionInterface","Recoil\/Database\/ConnectionInterface.html","",
+                [        ]],["PackageInfo","Recoil\/Database\/PackageInfo.html","",
+                [        ]],["StatementInterface","Recoil\/Database\/StatementInterface.html","",
+                [        ]]        ]]        ]]        ]
 
